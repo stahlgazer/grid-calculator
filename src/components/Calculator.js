55e27881316bcac5b790821d44961cd1505468e3
Gavin S <stahlgavin@gmail.com>
2020-09-16 12:53:12 -0700

    basic functionality complete

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,10 +2,21 @@ import React, { useState } from "react";
 
 export default function Calculator() {
   const [result, setResult] = useState(0);
+  const [memory, setMemory] = useState("");
 
   const numClick = (e) => {
     if (result === 0) {
       setResult(e.target.value);
+    } else if (
+      e.target.value === "/" ||
+      e.target.value === "*" ||
+      e.target.value === "+" ||
+      e.target.value === "-"
+    ) {
+      setMemory(result + e.target.value);
+      setResult(0)
+    } else if (e.target.value === "=") {
+      setResult(eval(memory+result));
     } else {
       setResult(`${result}${e.target.value}`);
     }
@@ -17,12 +28,12 @@ export default function Calculator() {
 
   return (
     <div className="calculator">
-      <input type="number" value={result} placeholder="0" />
+      <input type="number" onChange={numClick} value={result} placeholder="0" />
       <div className="buttons-container">
         <button onClick={clearClick} className="calc-button clear">
           Clear
         </button>
-        <button value={`/`} className="calc-button">
+        <button value="/" onClick={numClick} className="calc-button">
           &divide;
         </button>
         <button value={7} onClick={numClick} className="calc-button">
@@ -34,7 +45,7 @@ export default function Calculator() {
         <button value={9} onClick={numClick} className="calc-button">
           9
         </button>
-        <button value={`*`} className="calc-button">
+        <button value="*" onClick={numClick} className="calc-button">
           &times;
         </button>
         <button value={4} onClick={numClick} className="calc-button">
@@ -46,7 +57,9 @@ export default function Calculator() {
         <button value={6} onClick={numClick} className="calc-button">
           6
         </button>
-        <button className="calc-button">&minus;</button>
+        <button value="-" onClick={numClick} className="calc-button">
+          &minus;
+        </button>
         <button value={1} onClick={numClick} className="calc-button">
           1
         </button>
@@ -56,13 +69,13 @@ export default function Calculator() {
         <button value={3} onClick={numClick} className="calc-button">
           3
         </button>
-        <button value={`+`} className="calc-button">
+        <button value="+" onClick={numClick} className="calc-button">
           +
         </button>
         <button value={0} onClick={numClick} className="calc-button zero">
           0
         </button>
-        <button value={`=`} className="calc-button equal">
+        <button value="=" onClick={numClick} className="calc-button equal">
           =
         </button>
       </div>
